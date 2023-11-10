d35dabdc354793a27c5436f960e96cf1319c58a3
Fedor <fedoravdeev3@gmail.com>
2023-11-10 12:54:02 +0700

    add vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { App, plugin } from '@inertiajs/inertia-vue3'
+// import { InertiaProgress } from '@inertiajs/progress'
+
+// InertiaProgress.init()
+
+const el = document.getElementById('app')
+
+console.log('!!!')
+
+createApp({
+  render: () => h(App, {
+    initialPage: JSON.parse(el.dataset.page),
+    resolveComponent: name => import(`./Pages/${name}.vue`).then(module => module.default),
+  })
+}).use(plugin).mount(el)
